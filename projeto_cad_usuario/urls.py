b2021e7bf7c5daad94e8811442fea17ce9d8e34e
Job Ferreira <jobin.ferreira@gmail.com>
2024-07-08 15:16:22 -0300

    First commit

---
+++ projeto_cad_usuario/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from app_cad_usuarios import views
+
+urlpatterns = [
+    # rota, view responsavel, nome de referência
+    #usuarios.com
+    path('', views.home, name='home'),
+    #usuarios.com/usuarios
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
+]
